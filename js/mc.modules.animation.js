b3ac0b4b67e4932e8e993b70c37903205df44145
Mangoliciouz <31861001+Mangoliciouz@users.noreply.github.com>
2019-10-03 22:55:20 +0100

    WIP

--- js/mc.modules.animation.js
+++ js/mc.modules.animation.js
@@ -14,13 +14,9 @@ var menuHoverActive         = true; /* it will change to false if menuActive == 
 var menuTextOutColor        = "#777777";
 
 var customPageStart         = false;
-<<<<<<< HEAD
-var customPageStartURL      = "#home_layout_1.html";
-=======
-var customPageStartURL      = "#home.html";
->>>>>>> parent of 033bfda... WIP
+var customPageStartURL      = "#portfolio.html";
 var customPageStartSide     = "none";
-var customPageStartType     = "slideshow";
+var customPageStartType     = "full_width_gallery";
 
 var phpUrlPath              = "php/contact_synergy.php";
 var contactFormDemo         = true; /* SET IT TO FALSE FOR CONTACT FORM TO WORK */
